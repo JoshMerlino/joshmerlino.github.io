bd38f685a97ed73272b20dea63705adc00df6608
Josh Merlino <joshua.merlino@gmail.com>
2022-03-03 19:17:39 -0500

    add api performance monitor

--- web/ts/views/Performance.tsx
+++ web/ts/views/Performance.tsx
@@ -2,67 +2,90 @@
 import Photon from "photoncss";
 import { Card, CardTitle, Col, Container, Icon, Row, Spinner, Toolbar, ToolbarActions, ToolbarTitle, VHCenter } from "photoncss/lib/react";
 import React, { useEffect, useState } from "react";
+import useFetch from "runtime/util/hooks/useFetch";
 import ThemeSwitcher from "../components/ThemeSwitcher";
-
-import {
-  Chart,
-  Bars, Cloud, Dots, Labels, Lines, Pies, RadialLines, Ticks, Title,
-  Layer, Animate, Transform, Handlers,
-  DropShadow, Gradient
-} from 'rumble-charts';
+import Chart from "react-apexcharts";
+import { ApexOptions } from "apexcharts";
 
 export const route = "/performance";
 export const title = "Server Performance";
 
 export function PerformanceMonitor(): JSX.Element | null {
 
-	// Initialize default state
-	const [ state, setState ] = useState<API.RequestData>({ success: false });
-
 	// Resolve method
-	const resolve = (): any => fetch("https://joshmerlino.me/api/v2/request-data").then(resp => resp.json())
-		.then(setState)
-		.catch(() => setTimeout(resolve, 1000));
+	const [ state, refresh ] = useFetch<API.RequestData>("https://api.joshmerlino.me/v2/request-data");
 
 	// Set resolve on interval
-	useEffect(function() {
-		let __refresh = true;
-		(function refresh() {
-			if (__refresh) resolve().then(refresh);
-		}());
-		return function() {
-			__refresh = false;
-		};
-	}, []);
+	setTimeout(refresh, 60000);
 
-	if (state.success === false) return (
+	if (state === null || state.success === false || !state.history) return (
 		<VHCenter>
 			<Spinner/>
 		</VHCenter>
 	);
 
-	const series = {
-		data: state.history!.map((y, x) => ({ x, y }))
-	}
-
-	console.log(series);
-
 	return (
 		<Container>
 			<Row>
 
-				<Col sm={12} md={6} xl={4}>
-					<Card>
-						<CardTitle>API Requests</CardTitle>
-						<Chart
-							height={150}
-							width={600}
-							minY={0}
-							series={[series]}>
-							<Lines/>
-						</Chart>
-					</Card>
-				</Col>
+				<br />
+				<br />
+				<br />
+				<br />
+				<br />
+
+				<h2>API Requests / Second</h2>
+				<h3>Past hour</h3>
+				<div id="wrapper">
+					<Chart options={{
+						chart: {
+							id: "rps",
+							group: "social",
+							type: "line",
+							height: 160
+						},
+						stroke: {
+							curve: "smooth"
+						},
+						colors: [ "#2e93fa" ]
+					} as ApexOptions} series={[ {
+						name: "API Requests/second",
+						type: "area",
+						data: state.history
+							.map(obj => obj.req_per_second)
+							.map(n => Math.floor(n))
+							.map((y, key) => ({
+								y,
+								x: state.history!.length - key
+							}))
+					} ]} type="line" height={160} />
+					<br/>
+					<h2>Average Response Time (ms)</h2>
+					<h3>Past hour</h3>
+					<Chart options={{
+						chart: {
+							id: "rt",
+							group: "social",
+							type: "line",
+							height: 160
+						},
+						stroke: {
+							curve: "smooth"
+						},
+						colors: [ "#00e396" ]
+					} as ApexOptions} series={[ {
+						name: "API Response Time",
+						type: "area",
+						data: state.history
+							.map(obj => obj.response_time)
+							.map(n => Math.floor(n * 1000) / 1000)
+							.map((y, key) => ({
+								y,
+								x: state.history!.length - key
+							}))
+					} ]} type="line" height={160} />
+
+				</div>
 
 			</Row>
 		</Container>
